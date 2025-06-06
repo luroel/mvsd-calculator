83fe301f94c116ecd61e11d840db27dcf0d1afe2
luroel <jl.rodrigueze@hotmail.com>
2025-06-05 19:31:10 -0600

    Cambio de nombre de funcion array a calculate y devuelve la lista a en un vector np llamado c de dimension 3x3

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,11 +2,9 @@ import numpy as np
 
 a = ([0,1,2,3,4,5,6,7,8])
 
-L = np.array([a])
-print(L)
+def calculate(b):
+    c = np.array([b])
+    return print(c.reshape(3,3)) 
 
-def array(b):
-    return print(np.array([b]))
-
-array(a)
+calculate(a)
 
