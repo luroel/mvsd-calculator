8fdf7529a453db07315ad4300fc7e287223c6544
luroel <jl.rodrigueze@hotmail.com>
2025-06-05 22:02:54 -0600

    Cambiando nombre de variables

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-a = ([0,1,2,3,4,5,6,7,8])
+list = ([0,1,2,3,4,5,6,7,8])
 
-def calculate(b):
-    c = np.array([b])
-    return print(c.reshape(3,3)) 
+def calculate(list):
+    mvs_array = np.array([list])
+    return print(mvs_array.reshape(3,3)) 
 
-calculate(a)
+calculate(list)
 
