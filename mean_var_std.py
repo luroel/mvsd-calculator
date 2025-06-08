5c8026f198f769d00f322277706bc8defb2c6ffc
luroel <jl.rodrigueze@hotmail.com>
2025-06-07 19:50:55 -0600

    Creando axis_1 con el mean de las columnas, axis_2 con el mean de las filas y flatten con el mean de todo los valores de la matriz mvs_array

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 list = ([0,1,2,3,4,5,6,7,8])
+mvs_array = np.array([list]).reshape(3,3)
 
-def calculate(list):
-    mvs_array = np.array([list])
-    return print(mvs_array.reshape(3,3)) 
+def calculate(mvs_array):
+    mvs_array = np.array([list]).reshape(3,3)
+    axis_1 = mvs_array.mean(axis=0) #mean rows
+    axis_2 = mvs_array.mean(axis=1) #mean colums
+    flatten = mvs_array.mean()
+    
+    return print(axis_1, axis_2, flatten)
 
-calculate(list)
+calculate(mvs_array)
 
