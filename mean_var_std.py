11c59aeafe5a021bade79e1ef5c1d006d5cdaa77
luroel <jl.rodrigueze@hotmail.com>
2025-06-07 20:20:15 -0600

    Agregando varianza, desviacion estandar, max, min, sum

--- mean_var_std.py
+++ mean_var_std.py
@@ -12,15 +12,29 @@ def calculate(mvs_array):
     axvar_2 = mvs_array.var(axis=1)
     array_var = mvs_array.var()
     
+    axstd_1 = mvs_array.std(axis=0)
+    axstd_2 = mvs_array.std(axis=1)
+    array_std = mvs_array.std()
     
+    axmax_1 = mvs_array.max(axis=0)
+    axmax_2 = mvs_array.max(axis=1)
+    array_max = mvs_array.max()
     
+    axmin_1 = mvs_array.min(axis=0)
+    axmin_2 = mvs_array.min(axis=1)
+    array_min = mvs_array.max()
+    
+    axsum_1 = mvs_array.sum(axis=0)
+    axsum_2 = mvs_array.sum(axis=1)
+    array_sum = mvs_array.sum()
+        
     dic = {
         'mean': [axmean_1.tolist(), axmean_2.tolist(), float(array_mean)],
         'Variance': [axvar_1.tolist(), axvar_2.tolist(), float(array_var)],
-        'Standar deviation': [],
-        'Max': [],
-        'Min': [],
-        'Sum': []             
+        'Standar deviation': [axstd_1.tolist(), axstd_2.tolist(), float(array_std)],
+        'Max': [axmax_1.tolist(), axmax_2.tolist(), float(array_max)],
+        'Min': [axmin_1.tolist(), axmin_2.tolist(), float(array_min)],
+        'Sum': [axsum_1.tolist(), axsum_2.tolist(), float(array_sum)]             
     }
     
     return dic
