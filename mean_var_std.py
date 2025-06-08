8521f319cbe1adf9ad48e43426001b9ff76d7713
luroel <jl.rodrigueze@hotmail.com>
2025-06-07 20:11:06 -0600

    Cambio de nombre variables y agregando varianza

--- mean_var_std.py
+++ mean_var_std.py
@@ -4,12 +4,27 @@ list = ([0,1,2,3,4,5,6,7,8])
 mvs_array = np.array([list]).reshape(3,3)
 
 def calculate(mvs_array):
-    mvs_array = np.array([list]).reshape(3,3)
-    axis_1 = mvs_array.mean(axis=0) #mean rows
-    axis_2 = mvs_array.mean(axis=1) #mean colums
-    flatten = mvs_array.mean()
+    axmean_1 = mvs_array.mean(axis=0) #mean rows
+    axmean_2 = mvs_array.mean(axis=1) #mean colums
+    array_mean = mvs_array.mean()
     
-    return print(axis_1, axis_2, flatten)
+    axvar_1 = mvs_array.var(axis=0)
+    axvar_2 = mvs_array.var(axis=1)
+    array_var = mvs_array.var()
+    
+    
+    
+    dic = {
+        'mean': [axmean_1.tolist(), axmean_2.tolist(), float(array_mean)],
+        'Variance': [axvar_1.tolist(), axvar_2.tolist(), float(array_var)],
+        'Standar deviation': [],
+        'Max': [],
+        'Min': [],
+        'Sum': []             
+    }
+    
+    return dic
 
-calculate(mvs_array)
+dic = calculate(mvs_array)
+print(dic)
 
